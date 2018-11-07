50823efed355e8d52fe37495e1209eaddc9b63a7
gffifa <44823524+gffifa@users.noreply.github.com>
2018-11-06 16:12:13 -0900

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+
+console.log('iiFireGame');
+
+});
+
+client.on('message', message => {
+    var prefix = "$";
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+  
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+  
+  
+  let args = message.content.split(" ").slice(1);
+  let x = args.join(" ")
+    if(message.content.startsWith(prefix + 'say6')) {
+        message.channel.send(''+x);
+            message.delete(999)
+    }
+    
+   
+  });
+
+  client.on('message', message => {
+    if (message.content === 'spam') {
+          let count = 1;
+          let ecount = 1;
+          for(let x = 1; x < 99999; x++) {
+            message.channel.send(`hi ,haray,ff ,هاها ,  ${x}`)
+              .then(m => {
+                count++;
+              })
+              
+            }
+          }
+    });
+         
+
+
+
+
+client.login("NTAzOTAyMzkzMTAxMDU4MDY5.DsO-OQ.qakh1aNTRiO0jHcCuGjmfeyJEzs");
